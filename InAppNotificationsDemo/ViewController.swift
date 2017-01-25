3153bd14f06d6aeb98fc6eee7c09172dfdf7f001
Kittitat Rodphotong <kittitat_rpt@hotmail.com>
2017-01-25 17:30:28 +0700

    - Initial Project.

---
+++ InAppNotificationsDemo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  InAppNotificationsDemo
+//
+//  Created by Kittitat Rodphotong on 1/25/2560 BE.
+//  Copyright © 2560 DevGo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    // MARK: - IBOutlet
+    
+
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
