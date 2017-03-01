43d4f07df13f4aaef85552ac7108f1bb1fedbfe6
Kittitat Rodphotong <kittitat_rpt@hotmail.com>
2017-03-01 09:45:16 +0700

    - Create `Repeat Week Day` notification.

--- InAppNotificationsDemo/ViewController.swift
+++ InAppNotificationsDemo/ViewController.swift
@@ -22,7 +22,20 @@ class ViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        localNotification()
+        let c = NSDateComponents()
+        c.hour = 23
+        c.minute = 55
+
+        
+        // Get NSDate given the above date components
+        let date = NSCalendar(identifier: NSCalendar.Identifier.gregorian)?.date(from: c as DateComponents)
+        
+        print(date!)
+        
+//        scheduleLocalNoti(date: setNotificationDay(today: date! as NSDate, selectedDay: 3))
+        
+        let notifications = UIApplication.shared.scheduledLocalNotifications
+        print(notifications ?? "NO NOTIFICATION")
     }
     
     // MARK: - Function
@@ -39,7 +52,7 @@ class ViewController: UIViewController {
         // create a corresponding local notification
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy/MM/dd HH:mm"
-        let someDateTime = formatter.date(from: "2017/02/27 14:01")
+        let someDateTime = formatter.date(from: "2017/02/27 21:12")
         
         let notification = UILocalNotification()
         notification.alertBody = "Title"
@@ -49,5 +62,48 @@ class ViewController: UIViewController {
         
         UIApplication.shared.scheduleLocalNotification(notification)
     }
+    
+    // Local Notification Repeat Day
+    func scheduleLocalNoti(date: NSDate) {
+        let dayDic = [1:"Sunday",
+                      2:"Monday",
+                      3:"Tuesday",
+                      4:"Wednesday",
+                      5:"Thursday",
+                      6:"Friday",
+                      7:"Saturday"]
+        
+        let calendar = NSCalendar.current
+        let weekday = calendar.component(.weekday, from: date as Date)
+        
+        print("DATE: \(date)")
+        
+        let notification = UILocalNotification()
+        notification.fireDate = date as Date
+        notification.repeatInterval = .weekOfYear
+        notification.alertTitle = String(format: "%@'s Workout", dayDic[weekday]!)
+        notification.alertBody = String(format: "This is the message from %@'s workout", dayDic[weekday]!)
+        notification.alertAction = "Snooze"
+        notification.category = "workoutReminderID"
+        notification.soundName = UILocalNotificationDefaultSoundName
+        UIApplication.shared.scheduleLocalNotification(notification)
+    }
+    
+    func setNotificationDay(today: NSDate, selectedDay: Int) -> NSDate {
+        let daysToAdd: Int!
+        let calendar = NSCalendar.current
+        let weekday = calendar.component(.weekday, from: today as Date)
+        
+        if weekday > selectedDay {
+            daysToAdd = (7 - weekday) + selectedDay
+        } else {
+            daysToAdd = (selectedDay - weekday)
+        }
+        
+        let newDate = calendar.date(byAdding: .weekday, value: daysToAdd, to: today as Date)
+        
+        return newDate! as NSDate //if you don't have a date it'll crash
+    }
+
 }
 
